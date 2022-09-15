989519d973e3ee1b1963128b4aba8c0412b8a52a
ianlee02 <>
2022-09-15 09:38:06 -0500

    Changing shape

--- wallArt.js
+++ wallArt.js
@@ -6,6 +6,7 @@ shapeX = 50;
 shapeY = 50;
 speedY = 5;
 speedX = 5;
+shapeValue = 0;
 
 console.log("This is my first console log!!");
 
@@ -13,11 +14,17 @@ function draw() {
     fill(random(255), random(255), random(255));
     shapeX = shapeX + speedX;
     shapeY = shapeY + speedY;
-    circle(shapeX, shapeY, 50);
+    if (shapeValue == 0) {
+        circle(shapeX, shapeY, 50);
+    } else {
+        square(shapeX, shapeY, 50);
+    }
     if (shapeY > windowHeight - 50 || shapeY < 25) {
         speedY = -speedY;
+        shapeValue = 1 - shapeValue;
     }
     if (shapeX > windowWidth - 50 || shapeX < 25) {
         speedX = -speedX;
+        shapeValue = 1 - shapeValue;
     }
 }
