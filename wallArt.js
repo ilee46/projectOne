32b9881ea2b1cc15da6ee75330e2154407835f3b
ianlee02 <>
2022-09-20 08:12:53 -0500

    Added mouse feature

--- wallArt.js
+++ wallArt.js
@@ -11,7 +11,11 @@ shapeValue = 0;
 console.log("This is my first console log!!");
 
 function draw() {
-    fill(random(255), random(255), random(255));
+    if (mouseIsPressed === true) {
+        fill(0,0,0);
+      } else {
+        fill(random(255), random(255), random(255));
+      }
     shapeX = shapeX + speedX;
     shapeY = shapeY + speedY;
     if (shapeValue == 0) {
